d8c024a36bf4f101d33bd7df41ac481f0ca80cc6
Ashish Langhe <95478770+Ashish-Langhe@users.noreply.github.com>
2021-12-23 21:49:15 +0530

    Initial Commit

---
+++ DashboardUI/DashboardUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DashboardUIApp.swift
+//  DashboardUI
+//
+//  Created by user208584 on 12/23/21.
+//
+
+import SwiftUI
+
+@main
+struct DashboardUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
